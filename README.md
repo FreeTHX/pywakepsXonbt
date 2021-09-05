89fc8d5beef4c9696a7ed63b7b5f1a26d2b69333
FreeTHX <freethx.dev@gmail.com>
2021-09-05 22:09:51 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Py Wake Playstation X (ps3, ps4, ps5) On BlueTooth
+This is a simple Python library to switch on a Sony ps3/ps4/ps5 using Bluetooth
+This version extend the previous one developped for PlayStation 4 only.
+
+The main objective is to include it into [Home Assistant](https://www.home-assistant.io/ "Home Assistant") components to provide a "Wake PsX On Bt" service.
+
+## How does it work ?
+The Playstation with bluetooth controler can be simply switched on via :
+- Spoofing a previously paired controler (SixAxis/DualShock3, DualShock4 or DualSense) BDADDR
+- Initiate a HCI connection to the PlayStation BDADDR
+
+## Supported Devices
+Support of BDADDR spoofing is limited.
+The module tests the adapter support via a ```hci_read_local_version``` python implementation and read the ```manufacturer``` returned value (bytes[5:7])  
+Currently supported adapters :
+- Broadcom devices (```manufacturer = 15```)
+
+## Get Bluetooth addresses over USB
+Plug the DualShock3/4 or DualSense controler on your computer using micro USB cable.  
+Once ```pywakepsXonbt``` is installed, run the following command from a python shell:
+```python
+>>> import wakepsXonbt
+>>> wakepsXonbt.get_bt_addr()
+{'type': 'dualshock3'/'dualshock4'/'dualsense', 'dsbt_address': '00:1F:E2:12:34:56', 'psXbt_address': '90:CD:B6:12:34:56'}
+```
---
+++ wakepsXonbt/wakepsXonbt.py
@@ -0,0 +1,214 @@
+ADAPTER_SUFFIX = 'hci'
+ADAPTER_DEFAULT = 0
+IDVENDOR_SONY = [ 0x54c ]
+IDPRODUCT_DS = {  'dualshock3' : [0x0268],
+                  'dualshock4' : [0x05c4, 0x09cc],
+                  'dualsense'  : [0x0ce6] }
+
+def get_devid_from_devname(adaptername) -> int:
+    if isinstance(adaptername, int):
+        return adaptername
+    if isinstance(adaptername, str):
+        try:
+            adapter = int(adaptername.replace(ADAPTER_SUFFIX, ''))
+        except:
+            adapter = ADAPTER_DEFAULT
+    else:
+        adapter = ADAPTER_DEFAULT    
+    return adapter
+
+def hci_read_local_version_compid(adapter: int) -> int: 
+    import bluetooth._bluetooth as _bt
+
+    try:
+        hci_sock = _bt.hci_open_dev(adapter)
+        res = _bt.hci_send_req(hci_sock,
+                               _bt.OGF_INFO_PARAM,
+                               _bt.OCF_READ_LOCAL_VERSION,
+                               _bt.EVT_CONN_COMPLETE,
+                               9)
+        hci_sock.close()
+        # Check first byte.
+        # See read_local_version_rp.status
+        # 0 if OK or ERRNO if an error has been raised 
+        if res[0]:
+            # Invalid response
+            return -1
+            
+        return int.from_bytes(res[5:7],byteorder='little', signed=False)
+
+    except _bt.error as e:
+        raise BluetoothError(*e.args)
+
+def read_local_bdaddr(adapter: int) -> str: 
+    import struct
+    import bluetooth._bluetooth as _bt
+
+    try:
+        hci_sock = _bt.hci_open_dev(adapter)
+
+        old_filter = hci_sock.getsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, 14)
+        flt = _bt.hci_filter_new()
+        opcode = _bt.cmd_opcode_pack(_bt.OGF_INFO_PARAM, _bt.OCF_READ_BD_ADDR)
+        _bt.hci_filter_set_ptype(flt, _bt.HCI_EVENT_PKT)
+        _bt.hci_filter_set_event(flt, _bt.EVT_CMD_COMPLETE)
+        _bt.hci_filter_set_opcode(flt, opcode)
+        hci_sock.setsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, flt )
+
+        _bt.hci_send_cmd(hci_sock, _bt.OGF_INFO_PARAM, _bt.OCF_READ_BD_ADDR )
+        pkt = hci_sock.recv(255)
+        status,raw_bdaddr = struct.unpack("xxxxxxB6s", pkt)
+        assert status == 0
+
+        t = [ "%02X" % int(b) for b in raw_bdaddr ]
+        t.reverse()
+        bdaddr = ":".join(t)
+
+        # restore old filter
+        hci_sock.setsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, old_filter )
+        hci_sock.close()
+        return bdaddr
+    except _bt.error as e:
+        raise BluetoothError(*e.args)
+
+# Only BCM ( raspberry and others) supported yet)
+def bcm_write_local_bdaddr(adapter: int, dsbtaddr: str) -> None:
+    import bluetooth._bluetooth as _bt
+
+    try:
+        # Open hci socket
+        hci_sock = _bt.hci_open_dev(adapter)
+
+        # Get bytes from bluetooth address
+        baddrtospoof = bytearray.fromhex(dsbtaddr.replace(":",""))
+        baddrtospoof.reverse()
+
+        # Send HCI request
+        cmd_pkt = bytes( baddrtospoof)
+
+        # BCM WRITE ADDR command
+        # https://github.com/pauloborges/bluez/blob/master/tools/bdaddr.c
+        _bt.hci_send_req(hci_sock,
+                         _bt.OGF_VENDOR_CMD,
+                         0x0001, #OCF_BCM_WRITE_BD_ADDR
+                         0x00000000,
+                         1000,
+                         cmd_pkt)
+
+        hci_sock.close()
+
+    except _bt.error as e:
+        raise BluetoothError(*e.args)
+
+
+def hci_cc(adapter: int, psXbtaddr: str) -> None:
+    import bluetooth._bluetooth as _bt
+
+    try:
+        hci_sock = _bt.hci_open_dev(adapter)
+
+        # Get bytes from bluetooth address
+        bapsXaddr = bytearray.fromhex(psXbtaddr.replace(":",""))
+        bapsXaddr.reverse()
+        # HCI CONNECT
+        # BLUETOOTH SPECIFICATION Version 5.0 | Vol 2, Part E page 774
+        # https://github.com/pauloborges/bluez/blob/master/tools/hcitool.c => cmd_cc
+        # https://github.com/pauloborges/bluez/blob/master/lib/hci.c => hci_create_connection
+        cmd_pkt = bytes(bapsXaddr) + bytes([ # Target BTADDR on 6 bytes reverse order,
+                        0x18, 0xCC, # pkt_type HCI_DM1 | HCI_DM3 | HCI_DM5 | HCI_DH1 | HCI_DH3 | HCI_DH5
+                        0x02, # pscan_rep_mode
+                        0x00, # pscan_mode (reserved)
+                        0x00, 0x00, # clock_offset
+                        0x01]) # role_switch
+
+        # Send HCI request
+        _bt.hci_send_req(hci_sock,
+                        _bt.OGF_LINK_CTL,
+                        _bt.OCF_CREATE_CONN,
+                        _bt.EVT_CONN_COMPLETE,
+                        1000,
+                        cmd_pkt)
+        hci_sock.close()
+
+    except _bt.error as e:
+        raise BluetoothError(*e.args)
+
+
+write_local_bdaddr = { 15 : bcm_write_local_bdaddr}
+
+def send_magic_packet(adapter,
+                      psX_addr: str,
+                      ds_addr: str) -> None:
+
+    adapterdevid = get_devid_from_devname(adapter)
+    compid = hci_read_local_version_compid(adapterdevid)
+    if compid not in write_local_bdaddr:
+        return False
+
+    """Send magic packet to wake up a device."""
+    # Get the original addr
+    original_bdaddr = read_local_bdaddr(adapterdevid)
+    # Spoof the addr
+    write_local_bdaddr[compid](adapterdevid, ds_addr)
+    # connect to the PsX HCI CC
+    hci_cc(adapterdevid, psX_addr)
+    # Write back the original addr
+    write_local_bdaddr[compid](adapterdevid,original_bdaddr)
+
+    return True
+
+def get_bt_addr() -> dict:
+    import usb.core, usb.util
+
+    dev = None
+    product = None
+    for vendor in IDVENDOR_SONY:
+        for dstype in IDPRODUCT_DS.keys():
+            for product in IDPRODUCT_DS[dstype]:
+                dev = usb.core.find(idVendor=vendor, idProduct=product )
+                if dev is not None:
+                    break
+            if dev is not None:
+                break
+        if dev is not None:
+            break
+
+    # Sony controler not found !
+    if dev is None:
+        return None
+    
+    # Following pyusb recommendation, we detach (if needed) and claim interface
+    # https://github.com/pyusb/pyusb/blob/master/docs/tutorial.rst
+    reattach = False
+    if dev.is_kernel_driver_active(0):
+        reattach = True
+        dev.detach_kernel_driver(0)
+    usb.util.claim_interface(dev, 0)
+
+    dsbt_address = None
+    psXbt_address = None
+    if dstype == 'dualshock3':
+        # Read DS3 BT addresses with USB control transfer GET FEATURE REPORT on 0xf2'
+        msg = dev.ctrl_transfer(0xA1, 0x01, 0x3f2, 0x0000, 17)
+        dsbt_address = format(msg[4], '02X') + ':' + format(msg[5], '02X') + ':' + format(msg[6], '02X') + ':' + format(msg[7], '02X') + ':' + format(msg[8], '02X') + ':' + format(msg[9], '02X') 
+        # Read PlayStation X BT addresses with USB control transfer GET FEATURE REPORT on 0xf5'
+        msg = dev.ctrl_transfer(0xA1, 0x01, 0x3f5, 0x0000, 8)
+        psXbt_address   = format(msg[2], '02X') + ':' + format(msg[3], '02X') + ':' + format(msg[4], '02X') + ':' + format(msg[5], '02X') + ':' + format(msg[6], '02X') + ':' + format(msg[7], '02X') 
+    elif dstype == 'dualshock4':
+        # Read PS4 et DS BT addresses with USB control transfer GET FEATURE REPORT on 0x12'
+        msg = dev.ctrl_transfer(0xA1, 0x01, 0x0312, 0x0000, 0x0010)        
+        # Get the DualShock BT Address
+        dsbt_address = format(msg[6], '02X') + ':' + format(msg[5], '02X') + ':' + format(msg[4], '02X') + ':' + format(msg[3], '02X') + ':' + format(msg[2], '02X') + ':' + format(msg[1], '02X')
+        # Get the Playstation 4 BT Address                  
+        psXbt_address = format(msg[15], '02X') + ':' + format(msg[14], '02X') + ':' + format(msg[13], '02X') + ':' + format(msg[12], '02X') + ':' + format(msg[11], '02X') + ':' + format(msg[10], '02X')
+    elif dstype == 'dualsense':
+        msg = dev.ctrl_transfer(0xA1, 0x01, 0x309, 0x0000, 20)
+        dsbt_address = format(msg[6], '02X') + ':' + format(msg[5], '02X') + ':' + format(msg[4], '02X') + ':' + format(msg[3], '02X') + ':' + format(msg[2], '02X') + ':' + format(msg[1], '02X') 
+        psXbt_address = format(msg[15], '02X') + ':' + format(msg[14], '02X') + ':' + format(msg[13], '02X') + ':' + format(msg[12], '02X') + ':' + format(msg[11], '02X') + ':' + format(msg[10], '02X') 
+
+    # Clean disposal of device
+    usb.util.release_interface(dev,0)
+    if reattach:
+        dev.attach_kernel_driver(0)
+
+    return {'type': dstype, 'dsbt_address':dsbt_address, 'psXbt_address':psXbt_address }
