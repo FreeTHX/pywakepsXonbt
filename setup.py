99e6b35a171b05118f707ee020592523001a8542
FreeTHX <freethx.dev@gmail.com>
2021-09-05 22:11:35 +0200

    Add python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     keywords='wake playstation (ps3, ps4, ps5) on bt home assistant',
